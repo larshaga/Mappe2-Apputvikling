20bbe1b42ea0148151618a98f76f048f2fb5d803
LarsKristianHaga <LarsKHaga@gmail.com>
2017-10-25 14:01:36 +0200

    Can now send messages and save to database

--- app/src/main/java/com/example/larsh/mappe2s305357/NewMessageActivity.java
+++ app/src/main/java/com/example/larsh/mappe2s305357/NewMessageActivity.java
@@ -8,14 +8,19 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Calendar;
 import java.util.List;
 
 public class NewMessageActivity extends AppCompatActivity
 {
 
     DBHandler db;
+    DBHandlerMSG db_MSG;
+    Button visAlle;
+    TextView utskrift;
 
     @Override
     protected void onCreate( Bundle savedInstanceState )
@@ -25,6 +30,7 @@ public class NewMessageActivity extends AppCompatActivity
         setContentView(R.layout.activity_new_message);
 
         db = new DBHandler(this);
+        db_MSG = new DBHandlerMSG(NewMessageActivity.this);
 
         Toolbar topToolbar = (Toolbar) findViewById(R.id.top_toolbar);
         setSupportActionBar(topToolbar);
@@ -33,6 +39,26 @@ public class NewMessageActivity extends AppCompatActivity
 
         Button sendNewMessage = (Button) findViewById(R.id.btn_sendMessage);
         final EditText messageText = (EditText) findViewById(R.id.newMessageText);
+        visAlle = (Button) findViewById(R.id.visalle);
+        utskrift = (TextView) findViewById(R.id.utskrift);
+        visAlle.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick( View view )
+            {
+                List<Message> testing = db_MSG.findAllMessages();
+
+                String tekst = "";
+
+                for (Message kontakt : testing)
+                {
+                    tekst = tekst + "Id: " + kontakt.getMessage_ID() + ", " + kontakt.getMessagePhonenumber() + ", " + kontakt.getMessageMessage() + ", " + kontakt.getMessageDate() + ", " + kontakt.getMessageStatus();
+                    Log.d("Test: ", tekst);
+                }
+                utskrift.setText(tekst);
+            }
+        });
 
         sendNewMessage.setOnClickListener(new View.OnClickListener()
         {
@@ -58,6 +84,17 @@ public class NewMessageActivity extends AppCompatActivity
     public void sendSMS( String phoneNumber, String message )
     {
 
+        Calendar savingDate = Calendar.getInstance();
+        int messageDate = savingDate.get(Calendar.DATE);
+        int messageMonth = savingDate.get(Calendar.MONTH);
+        int messageYear = savingDate.get(Calendar.YEAR);
+        int messageHour = savingDate.get(Calendar.HOUR_OF_DAY);
+        int messageMinute = savingDate.get(Calendar.MINUTE);
+        int messageMillis = savingDate.get(Calendar.MILLISECOND);
+
+        String status = "Not sent";
+        String date = messageDate + "/" + messageMonth + "-" + messageYear + ", " + messageHour + ":" + messageMinute + ":" + messageMillis;
+
         try
         {
             Log.i("Phonenumber", phoneNumber);
@@ -65,6 +102,8 @@ public class NewMessageActivity extends AppCompatActivity
             SmsManager smsManager = SmsManager.getDefault();
             smsManager.sendTextMessage(phoneNumber, null, message, null, null);
             Toast.makeText(NewMessageActivity.this, "Message sent", Toast.LENGTH_SHORT).show();
+
+            db_MSG.newMessage(phoneNumber,message,date,status);
         } catch (Exception e)
         {
             Toast.makeText(NewMessageActivity.this, e.getMessage().toString(), Toast.LENGTH_LONG).show();
